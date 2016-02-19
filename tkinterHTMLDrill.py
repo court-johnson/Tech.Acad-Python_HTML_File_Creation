57490d97d83c9048c3c8dc6b2d2415e1afe6554d
Courtney Johnson <court.johnson1221@gmail.com>
2016-02-19 12:40:13 -0600

    Database functionality added
    Created a database that stores previous entries so the user can either
    create a new entry for the body text or select a previous entry for new
    use.

--- tkinterHTMLDrill.py
+++ tkinterHTMLDrill.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import tkinter.messagebox
 import webbrowser
+import sqlite3
 import os.path
 #------------------------------------------------------------------------------------------
 #html script to be created 
@@ -12,6 +13,21 @@ script = """<html>
 </html>"""
 
 #------------------------------------------------------------------------------------------
+
+#connecting to the already created database and table
+conn = sqlite3.connect('htmlDrill.db')
+c = conn.cursor()
+
+#Table already exists currently
+#c.execute("DROP TABLE IF EXISTS entriesHTML")
+#c.execute("CREATE TABLE entriesHTML(ID INTEGER PRIMARY KEY AUTOINCREMENT, ENTRY TEXT)")
+
+#collecting data from when the last body text was created/saved 
+c.execute("SELECT ENTRY FROM entriesHTML")#WHERE ID =(SELECT MAX(ID) FROM entriesHTML)")
+previousEntries = c.fetchall()
+
+#------------------------------------------------------------------------------------------
+
 class Feedback():
 
     def __init__(self, master):
@@ -36,10 +52,16 @@ class Feedback():
         
         #button that binds to the create html file function
         self.createFile = ttk.Button(self.frame_content, text='Create to Web Browser', style='TButton')
-        self.createFile.grid(row = 2, column = 0, padx = 7, pady = 10, sticky = 'e')
+        self.createFile.grid(row = 3, column = 0, padx = 7, pady = 10, sticky = 'e')
         self.createFile.bind("<Button-1>", self.createButtonClicked)
+        
+        #button that allows the user to browser previous entries to reuse
+        self.pickOldEntry = ttk.Button(self.frame_content, text='View Previous Entries', style = 'TButton')
+        self.pickOldEntry.grid(row = 2, column = 0, padx = 7, pady = 10, sticky = 'e')
+        self.pickOldEntry.bind("<Button-1>", self.pickOptionClicked)
 
 #------------------------------------------------------------------------------------------
+        
     def createButtonClicked(self, event):
         #indicating to create a file under a particular name
         file = open("test.html", 'w')
@@ -50,7 +72,60 @@ class Feedback():
 
         #opening the file to the default web browser automatically
         webbrowser.open(os.path.abspath("test.html"))
+        self.dataInsert()
         file.close()
+        
+#------------------------------------------------------------------------------------------
+
+    def pickOptionClicked(self, event):
+        #creating new popup window to browser previously used body texts
+        self.popupBox = Toplevel()
+        self.popupBox.config(background = '#85adad')
+ 
+        self.pickEntrySelection = ttk.Label(self.popupBox, text = "Select a previous saved entry:", style = 'TLabel')
+        self.pickEntrySelection.grid(row = 0, column = 0, padx = 10, pady = 10, sticky = 'w')   
+
+        #box that produces previous used body text entries generated from the database
+        self.oldEntries = Listbox(self.popupBox, width = 35, height = 10, borderwidth = 3)
+        self.oldEntries.grid(row=1,column=0, padx = 10, sticky = 'w')
+
+        #referencing each individual entry in the database and remove the brackets from the data
+        for i in previousEntries:
+            self.oldEntries.insert(END, i[0])
+
+        #enters old entry selection into text box on initial window to be saved to the html file
+        self.submit = ttk.Button(self.popupBox, text='Submit', style = 'TButton')
+        self.submit.grid(row = 2, column = 0, padx = 10, pady = 10, sticky = 'w')
+        self.submit.bind('<Button-1>', self.oldEntryPicked)
+
+        #closing the popup window
+        self.cancel = ttk.Button(self.popupBox, text='Cancel', style = 'TButton')
+        self.cancel.grid(row = 2, column = 0, padx = 10, pady = 10, sticky = 'e')
+        self.cancel.bind('<Button-1>', self.cancelClose)
+        
+#------------------------------------------------------------------------------------------
+    #event from the submit button being clicked, inserts selected text to the original text box
+    def oldEntryPicked(self, event):
+        pickedEntry = self.oldEntries.selection_get()
+        self.enteredText.insert(END, pickedEntry)
+        self.popupBox.destroy()
+        
+    #closes pop up window   
+    def cancelClose(self, event):
+        self.popupBox.destroy()
+
+#------------------------------------------------------------------------------------------
+
+    #function for adding the entered text from the user into the database for later use or reference
+    def dataInsert(self):        
+        #inserting retrieved time and date of when the transfer button was clicked
+        c.execute("INSERT INTO entriesHTML (ENTRY) VALUES (?)", (self.enteredText.get('1.0', END),))
+        conn.commit()
+
+        #disconnecting from database
+        c.close()
+        conn.close()
+        
 #------------------------------------------------------------------------------------------
 
 def main():
